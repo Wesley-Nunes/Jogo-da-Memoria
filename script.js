36fa57be437f09d5e29d265e53b136f312ea03a0
Wesley-Nunes <wesnmonteiro@gmail.com>
2022-02-11 14:35:31 -0300

    refactor: Improve the quality of the names and formating

--- script.js
+++ script.js
@@ -1,67 +1,64 @@
-const cards = document.querySelectorAll('.card');
-let hasFlippedCard = false;
+const cards = document.querySelectorAll(".card");
+let hasCardFlip = false;
 let firstCard, secondCard;
 let lockBoard = false;
 
 function flipCard() {
-	if(lockBoard) return;
-	if(this === firstCard) return;
+  if (lockBoard || this === firstCard) return;
 
-	this.classList.add('flip');
-	if (!hasFlippedCard) {
-		hasFlippedCard = true;
-		firstCard = this;
-		return;
-	}
+  this.classList.add("flip");
+  if (!hasCardFlip) {
+    hasCardFlip = true;
+    firstCard = this;
+    return;
+  }
 
-	secondCard = this;
+  secondCard = this;
 
-	hasFlippedCard = false;
-	checkForMatch();
+  hasCardFlip = false;
+  checkForMatch();
 }
 
 function checkForMatch() {
-	if (firstCard.dataset.card === secondCard.dataset.card) {
-		disableCards();
-		return;
-	}
+  if (firstCard.dataset.card === secondCard.dataset.card) {
+    disableCards();
+    return;
+  }
 
-	unflipCards();
+  unflipCards();
 }
 
 function disableCards() {
-	firstCard.removeEventListener('click', flipCard);
-	secondCard.removeEventListener('click', flipCard);
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
 
-	resetBoard();
+  resetBoard();
 }
 
 function unflipCards() {
-	lockBoard = true;
+  lockBoard = true;
 
-	setTimeout(() => {
-		firstCard.classList.remove('flip');
-		secondCard.classList.remove('flip');
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
 
-		resetBoard();
-	}, 1500);
+    resetBoard();
+  }, 1500);
 }
 
 function resetBoard() {
-	[hasFlippedCard, lockBoard] = [false, false];
-	[firstCard, secondCard] = [null, null];
+  [hasCardFlip, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
 
-
 cards.forEach((card) => {
-	card.addEventListener('click', flipCard);
+  card.addEventListener("click", flipCard);
 });
-
 
 //IIFE
 (function shuffle() {
-	cards.forEach((card) => {
-		let randomPosition = Math.floor(Math.random() * 12);
-		card.style.order = randomPosition;
-	})
+  cards.forEach((card) => {
+    let randomPosition = Math.floor(Math.random() * 12);
+    card.style.order = randomPosition;
+  });
 })();
